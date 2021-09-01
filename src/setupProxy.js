3c085516ad35eda3f767edf067f71d3f9638f928
Samuel Cattini-Schultz <syriiins@gmail.com>
2021-09-01 21:33:51 +1000

    Remove setupProxy.js in favour of simple package.json "proxy" field

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = function (app) {
-    app.use(
-        createProxyMiddleware(["/osuauth", "/api", "/beatmapfiles"], {
-            target: "http://127.0.0.1:8000",
-        })
-    );
-};
