821bbd1f8b7343b30f24161e41462429a9a6c15c
Samuel Cattini-Schultz <syriiin@proton.me>
2026-07-18 22:03:00 +0900

    Add about to footer

--- src/Osuchan/Footer.tsx
+++ src/Osuchan/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const FooterWrapper = styled.footer`
@@ -6,9 +7,20 @@ const FooterWrapper = styled.footer`
     color: ${(props) => props.theme.colours.currant};
 `;
 
+const FooterLink = styled(Link)`
+    color: ${(props) => props.theme.colours.currant};
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
 const Footer = () => (
     <FooterWrapper>
         <span>&copy; osu!chan 2026</span>
+        <span> | </span>
+        <FooterLink to="/about">About</FooterLink>
     </FooterWrapper>
 );
 
